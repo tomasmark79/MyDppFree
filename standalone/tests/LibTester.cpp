6671ce48b5724359d483dab10f64569aedbda0ce
tomasmark79 <admin@digitalspace.name>
2025-05-02 15:29:06 +0200

    Committed by Pro Template 2025-05-02 15:29:07

---
+++ standalone/tests/LibTester.cpp
@@ -0,0 +1,20 @@
+// MIT License
+// Copyright (c) 2024-2025 Tomáš Mark
+
+#include "../src/AppCore.hpp"
+#include <gtest/gtest.h>
+
+TEST (AppLogic, HandlesArguments) {
+  const char* argv[] = { "DotNameStandalone", "--help" };
+  EXPECT_EQ (runApp (2, argv), 0);
+}
+
+TEST (AppLogic, HandlesArgumentsNoLibrary) {
+  const char* argv[] = { "DotNameStandalone", "--omit" };
+  EXPECT_EQ (runApp (2, argv), 0);
+}
+
+TEST (AppLogic, HandlesArgumentsLog2File) {
+  const char* argv[] = { "DotNameStandalone", "--log2file" };
+  EXPECT_EQ (runApp (2, argv), 0);
+}
